06fc06a85670ba5c4be4a85a385036277f108022
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2023-03-28 08:35:15 +0600

    f commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App () {
+  return (
+    <div>
+      <h1 className='color-h'>Instagram</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout = () => {
+   return (
+      <div>
+
+         <main>
+
+         </main>
+      </div>
+   );
+};
+
+export default Layout;
